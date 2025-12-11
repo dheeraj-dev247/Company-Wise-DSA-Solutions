9c16a5fedb4c12506d49bf79b9b1af5836c9b3c6
dheeraj-dev247 <dheeraj.dev247@gmail.com>
2025-12-11 23:18:58 +0530

    need to finish this

--- 01 Initial Startups/01 Arrays/05_First_Missing_Positive.py
+++ 01 Initial Startups/01 Arrays/05_First_Missing_Positive.py
@@ -1,1 +1,10 @@
-def
+def first_missing_positive_simple(nums):
+    n = len(nums)
+
+    # Step 1: Replace negative numbers and numbers > n with a placeholder
+    for i in range(n):
+        if nums[i] <= 0 or nums[i] > n:
+            nums[i] = n + 1
+
+
+print(first_missing_positive_simple([3, 4, -1, 1]))
